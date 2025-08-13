b5d6c9e5b53feba7763de044561c48c1eb50f5a6
suhail khan <khansohail015@gmail.com>
2025-08-13 21:00:46 +0530

    setup

---
+++ js/index.js
@@ -0,0 +1,29 @@
+// Variables/
+
+// 1st way to decleare a variables
+// let name='suhail'
+// let age=25
+// let school='Integral University Lucknow'
+// console.log(name,age,school)
+
+
+// 2nd way to decleare a variables
+// var name="Ganesh"
+// var age=21
+// var city='Delhi'
+
+// console.log(name,age,city)
+
+
+// 3rd way to decleare a variables
+const name="Ganesh"
+const age=21
+const city='Delhi'
+
+console.log(name,age,city)
+
+
+
+
+
+
