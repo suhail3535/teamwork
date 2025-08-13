6f912b2e7b049c087b4b2cba396d247619e7124d
suhail khan <khansohail015@gmail.com>
2025-08-13 21:48:25 +0530

    class-01

--- README.md
+++ README.md
@@ -1,29 +1,9 @@
-# teamwork
-step-01
-how to clone a github repo in your local
-git clone https://github.com/suhail3535/teamwork.git
+Date: 13-08-2025
+Students' Task:
 
-step-02  run this command into your github repo
-how to push changes into github
-a)- git add . (it will add all you files you want to push into github)
-b)- git commit -m "project setup " (to add a commit on that files)
-c)- git push origin main/master
-
-------------------------
-How to create branches
-1- how to cheack all branches
-git branch 
-
-**how to craete a branch**
-2- git branch <name of branch>
-example:-git branch navbar 
-
-3-how to chekout in other branch
-git checkout <branch name>
-ex-git checkout navbar
-
------- sortcut for create and checkout----
-git checkout -b <branch name>
-example:-git checkout -b footer
-
-the above command will create a branch and checkout.
+Question 01: What is the difference between let, var, and const?
+Ans:-
+Question 02: How many types of data types are there?
+Question 03: What are variables, and how can they be declared?
+Question 04: What is the difference between null and undefined?
+Question 05: Explain all types of operators.
--- js/index.js
+++ js/index.js
@@ -16,14 +16,94 @@
 
 
 // 3rd way to decleare a variables
-const name="Ganesh"
-const age=21
-const city='Delhi'
+// const name="Ganesh"
+// const age=21
+// const city='Delhi'
 
-console.log(name,age,city)
+// console.log(name,age,city)
 
 
 
+// 2Datatypes
+
+// 1-primitive data types
+// A) string
+// let school="Integral University Lucknow"
+// B) number
+// let age=25
+// let rollno=12345
+// console.log(typeof age, typeof rollno)
+// C) boolean
+
+// let isStudent=true
+// let isTeacher=false
+// console.log(typeof isStudent, typeof isTeacher)
+// D) null
+// let name=null
+// console.log( typeof name)
+// // E) undefined
+// let city="Delhi"
+// console.log(city)
+
+// 2-non primitive data types
+// A) object
+// let object={
+// name:"Ganesh",
+// age:21,
+// city:"Delhi"
+// }
+
+// console.log(object) 
+
+// B) array
+// let array=[1,2,3,4,5,6,7,8,9,10]
+// console.log(array.length)
+
+
+
+// C) function
+// function add(a,b){
+//    return a+b
+// }
+// console.log(add(5,10))
+// console.log(add(25,10))
+// console.log(add(15,10))
+
+// // 2nd way to call a function
+// let result=add(100,200)
+// console.log(add())
+
+// 5. Operators
+// Arithmetic: +, -, *, /, %, **
+// let a = 2;
+// let b = 1;
+// console.log("Addition:", a + b); // Addition: 15
+// console.log("Subtraction:", a - b); // Subtraction: 5
+// console.log("Multiplication:", a * b); // Multiplication: 50
+// console.log("Division:", a / b); // Division: 2
+// console.log("Modulus:", a % b); // Modulus: 0
+// console.log("Exponentiation:", a** a); // Exponentiation: 100000
+
+
+
+
+
+// Assignment: =, +=, -=, *=
+// let x = "5" ; 
+// let y = 5;
+
+// console.log(x===y)
+
+// Comparison: ==, ===, !=, !==, >, <, >=, <=
+// let x = "5" ; 
+// let y = 5;
+
+// console.log(x!=y)
+
+// Logical: && (AND), || (OR), ! (NOT)
+ 
+
+// Increment/Decrement: ++, --
 
 
 
